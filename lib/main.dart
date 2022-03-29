a1f5023b155c31c169ab05e5dbd92a6e55645a37
Ven <venbeks@gmail.com>
2022-03-29 22:07:00 +0100

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Scaffold(appBar: AppBar(
-        title: Text("I am rich"),
-      ),),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I am rich'),
+        ),
+      ),
     ),
   );
 }
