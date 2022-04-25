906c4e5bd2b9a607bb4c19849c1026c04cd1644f
Ven <venbeks@gmail.com>
2022-04-25 17:12:19 +0100

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Image(
-          image:NetworkImage('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg')
+          image:NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg')
         ),
       ),
     ),
